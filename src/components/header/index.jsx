c52161de9eaceb1123d5604b2248b5b174ae7a3a
Fabian <jujescufabian@gmail.com>
2024-11-02 19:22:25 -0500

    arcade themed login

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -17,7 +17,6 @@ const Header = () => {
                     :
                     <>
                         <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
                     </>
             }
 
